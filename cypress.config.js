e294745128427fe3e61d51005d31c92ff88dc03f
Julia Martins <mjuh.ms@gmail.com>
2022-08-15 08:26:42 -0300

    Add reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
+    reporter: 'mochawesome',
+    reporterOptions: {
+        reportDir: 'cypress/results',
+        overwrite: false,
+        html: false,
+        json: true
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
